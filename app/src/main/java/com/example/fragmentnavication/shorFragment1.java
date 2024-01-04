cc48227280e2dc27bcb1f9adabce8377277cb85d
peep <thpwr123@gmail.com>
2024-01-04 10:09:33 +0700

    before lunch yay

--- app/src/main/java/com/example/fragmentnavication/shorFragment1.java
+++ app/src/main/java/com/example/fragmentnavication/shorFragment1.java
@@ -1,5 +1,7 @@
 package com.example.fragmentnavication;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.content.Context;
 import android.os.Bundle;
 
@@ -12,6 +14,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
@@ -25,16 +30,25 @@ public class shorFragment1 extends Fragment {
 
     int q, r, k, p, newq, key1, key2;
 
-    Log log;
 
-    //the variables for the preparation step
-    TextView selectPrimeNumber1, NumberPicker1, selectPrimeNumber2, NumberPicker2, Composite;
-    TextView ShorStep1, ShorStep1Explanation, SelectRandomK;
+
+    //set variables
+    Log log;
+    NumberPicker NumberPicker1, NumberPicker2;
+    TextView selectPrimeNumber1, selectPrimeNumber2, Composite;
+    TextView ShorStep1, ShorStep1Explanation;
     TextView Shor2point1, ShorStep2, ShorStep2Explanation, ShorStep2Formula, warning1, Shor2point2, warning2, Shor2point3;
-    TextView Step3point1, ShorStep3, ShorStep3Explanation, Shor3point2, warning3, Shor3point3;
+    TextView Shor3point1, ShorStep3, ShorStep3Explanation, Shor3point2, warning3, Shor3point3;
     TextView Shor4point1, ShorStep4, ShorStep4Explanation, Shor4point2, Shor4point3, Shor4point4, ShorStepFinal;
     Button ShorToHome1, ShorToHome2;
+    EditText SelectRandomK;
+    TableLayout tableLayout;
 
+
+    //set prime number strings
+    final String[]  primeNumbers= {"-", "13" ,"17" ,"19","23","29","31","37","41","43","47"
+            ,"53","59","61","67","71","73","79","83","89","97","101","103","107"
+    };;
 
 
 
@@ -44,59 +58,21 @@ public class shorFragment1 extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_shor1, container, false);
-        ShorToHome1 = view.findViewById(R.id.ShorToHome1);
-        ShorToHome2 = view.findViewById(R.id.ShorToHome2);
-        isAllFabsVisible=false;
-        setHideFab();
+        context = context.getApplicationContext();
+        view = inflater.inflate(R.layout.fragment_shor1, container, true);
+        loadObject();
+        //setHideFab();
+   //     setHideStep2();
+    //    setHideStep3();
+    //    setHideStep4();
 
-        mAddFab.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
 
-                        isAllFabsVisible=!isAllFabsVisible;
 
-                        if(isAllFabsVisible) {
-                            setShowFab();
-                        }else {
-                            setHideFab();
-                        }
 
-                    }
-                });
 
-        goToAttack.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        LaunchAttackFragment();
-                    }
-                });
 
-        goToDefense.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        LaunchDefendFragment();
-                    }
-                });
-        backToHome.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        LaunchIntroFragment();
-                    }
-                });
 
-        goToShor.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        LaunchShorFragment();
 
-                    }
-                });
 
         ShorToHome1.setOnClickListener(
                 new View.OnClickListener() {
@@ -115,32 +91,16 @@ public class shorFragment1 extends Fragment {
                 });
 
 
-        view.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-
-
-                if(isAllFabsVisible) {
-                    setHideFab();
-                    isAllFabsVisible=!isAllFabsVisible;
-                }
-
-
-            }
-        });
-
-        return view;
-
+        return null;
     }
 
     View view;
-    Button nextBtn;
 
     FloatingActionButton backToHome, goToAttack, goToDefense,goToShor;
     ExtendedFloatingActionButton mAddFab;
     TextView directToHomeActionText, DirectToAttackActionText, DirectToDefenseActionText,DirectToShorActionText;
     // to check whether sub FABs are visible or not
-    Boolean isAllFabsVisible;
+
 
     private void replaceFragment(Fragment fragment){
 
@@ -150,67 +110,7 @@ public class shorFragment1 extends Fragment {
         fragmentTransaction.commit();
     }
 
-    private void setHideFab(){
-        mAddFab = view.findViewById(R.id.add_fab);
-        backToHome = view.findViewById(R.id.direct_to_home_fab);
-        goToAttack = view.findViewById(R.id.direct_to_attack_fab);
-        goToDefense = view.findViewById(R.id.direct_to_defense_fab);
-        goToShor = view.findViewById(R.id.direct_to_shor_fab);
-        directToHomeActionText =
-                view.findViewById(R.id.direct_to_home_action_text);
-        DirectToAttackActionText =
-                view.findViewById(R.id.direct_to_attack_action_text);
-        DirectToDefenseActionText =
-                view.findViewById(R.id.direct_to_defense_action_text);
-        DirectToShorActionText = view.findViewById(R.id.direct_to_shor_text);
-        // Now set all the FABs and all the action name
-        // texts as GONE
-        backToHome.setVisibility(View.GONE);
-        goToAttack.setVisibility(View.GONE);
-        goToDefense.setVisibility(View.GONE);
-        goToShor.setVisibility(View.GONE);
 
-        directToHomeActionText.setVisibility(View.GONE);
-        DirectToAttackActionText.setVisibility(View.GONE);
-        DirectToDefenseActionText.setVisibility(View.GONE);
-        DirectToShorActionText.setVisibility(View.GONE);
-
-        // make the boolean variable as false, as all the
-        // action name texts and all the sub FABs are
-        // invisible
-        //isAllFabsVisible = false;
-        // Set the Extended floating action button to
-        // shrinked state initially
-        mAddFab.shrink();
-
-    }
-
-    private void setShowFab(){
-
-        // when isAllFabsVisible becomes
-        // true make all the action name
-        // texts and FABs VISIBLE.
-        backToHome.show();
-        goToAttack.show();
-        goToDefense.show();
-        goToShor.show();
-        directToHomeActionText
-                .setVisibility(View.VISIBLE);
-        DirectToAttackActionText
-                .setVisibility(View.VISIBLE);
-        DirectToDefenseActionText
-                .setVisibility(View.VISIBLE);
-        DirectToShorActionText.setVisibility(View.VISIBLE);
-        // Now extend the parent FAB, as
-        // user clicks on the shrinked
-        // parent FAB
-        mAddFab.extend();
-
-        // make the boolean variable true as
-        // we have set the sub FABs
-        // visibility to GONE
-
-    }
 
     public void LaunchShorFragment() {
         replaceFragment(new shorFragment1());
@@ -224,4 +124,57 @@ public class shorFragment1 extends Fragment {
     public void LaunchDefendFragment() {
         replaceFragment(new DefendFragment1());
     }
+
+    public void loadObject() {
+
+        ShorToHome1 = view.findViewById(R.id.ShorToHome1);
+        ShorToHome2 = view.findViewById(R.id.ShorToHome2);
+
+        NumberPicker1 = (NumberPicker) view.findViewById(R.id.NumberPicker1);
+        NumberPicker1.setMinValue(0);
+        NumberPicker2 = (NumberPicker) view.findViewById(R.id.NumberPicker2);
+        NumberPicker2.setMinValue(0);
+
+        selectPrimeNumber1 = (TextView) view.findViewById(R.id.selectPrimeNumber1);
+        selectPrimeNumber2 = (TextView) view.findViewById(R.id.selectPrimeNumber2);
+        Composite = (TextView) view.findViewById(R.id.Composite);
+
+        ShorStep1 = (TextView) view.findViewById(R.id.ShorStep1);
+        ShorStep1Explanation = (TextView) view.findViewById(R.id.ShorStep1Explanation);
+
+        Shor2point1 = (TextView) view.findViewById(R.id.Shor2point1);
+        ShorStep2 = (TextView) view.findViewById(R.id.ShorStep2);
+        ShorStep2Explanation = (TextView) view.findViewById(R.id.ShorStep2Explanation);
+        ShorStep2Formula = (TextView) view.findViewById(R.id.ShorStep2Formula);
+        warning1 = (TextView) view.findViewById(R.id.warning1);
+        Shor2point2 = (TextView) view.findViewById(R.id.Shor2point2);
+        warning2 = (TextView) view.findViewById(R.id.warning2);
+        Shor2point3 = (TextView) view.findViewById(R.id.Shor2point3);
+
+        Shor3point1 = (TextView) view.findViewById(R.id.Shor3point1);
+        ShorStep3 = (TextView) view.findViewById(R.id.ShorStep3);
+        ShorStep3Explanation = (TextView) view.findViewById(R.id.ShorStep3Explanation);
+        Shor3point2 = (TextView) view.findViewById(R.id.Shor3point2);
+        warning3 = (TextView) view.findViewById(R.id.warning3);
+        Shor3point3 = (TextView) view.findViewById(R.id.Shor3point3);
+
+        Shor4point1 = (TextView) view.findViewById(R.id.Shor4point1);
+        ShorStep4 = (TextView) view.findViewById(R.id.ShorStep4);
+        ShorStep4Explanation = (TextView) view.findViewById(R.id.ShorStep4Explanation);
+        Shor4point2 = (TextView) view.findViewById(R.id.Shor4point2);
+        Shor4point3 = (TextView) view.findViewById(R.id.Shor4point3);
+        Shor4point4 = (TextView) view.findViewById(R.id.Shor4point4);
+        ShorStepFinal = (TextView) view.findViewById(R.id.ShorStepFinal);
+
+        view.findViewById(R.id.ShorToHome1);
+        view.findViewById(R.id.ShorToHome2);
+
+        SelectRandomK = view.findViewById(R.id.SelectRandomK);
+        tableLayout = view.findViewById(R.id.tableLayout);
+
+
+
+    }
+
+
 }
